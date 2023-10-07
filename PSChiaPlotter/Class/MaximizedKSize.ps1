f76031b91c1bbff022d857cb0526b20b071c605d
SESH01 <147237794+SESH01@users.noreply.github.com>
2023-10-07 12:21:49 +0400

    Update MaximizedKSize.ps1
    Added upto K40

--- PSChiaPlotter/Class/MaximizedKSize.ps1
+++ PSChiaPlotter/Class/MaximizedKSize.ps1
@@ -3,6 +3,11 @@ enum KSize{
     K33 = 33
     K34 = 34
     K35 = 35
+    K36 = 36
+    K37 = 37
+    K38 = 38
+    K39 = 39
+    K40 = 40
 }
 
 class MaximizedKSize {
@@ -12,6 +17,11 @@ class MaximizedKSize {
     [Decimal]$KSizeBytes
     [int64]$TotalBytes
 
+    static [Decimal]$K40 = 32824.3 * 1gb
+    static [Decimal]$K39 = 15931.8 * 1gb
+    static [Decimal]$K38 = 7732.8 * 1gb
+    static [Decimal]$K37 = 3753.3 * 1gb
+    static [Decimal]$K36 = 1821.7 * 1gb
     static [Decimal]$K35 = 884.1 * 1gb
     static [Decimal]$K34 = 429.8 * 1gb
     static [Decimal]$K33 = 208.8 * 1gb
@@ -25,6 +35,11 @@ class MaximizedKSize {
         $this.TotalBytes = $TotalBytes
 
         $this.KSizeBytes = switch ($this.KSize){
+            "K40" {[MaximizedKSize]::K40}
+            "K39" {[MaximizedKSize]::K39}
+            "K38" {[MaximizedKSize]::K38}
+            "K37" {[MaximizedKSize]::K37}
+            "K36" {[MaximizedKSize]::K36}
             "K35" {[MaximizedKSize]::K35}
             "K34" {[MaximizedKSize]::K34}
             "K33" {[MaximizedKSize]::K33}
