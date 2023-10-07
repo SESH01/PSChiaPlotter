9065a79e41166e9addf048b5885d7134399ce730
SESH01 <147237794+SESH01@users.noreply.github.com>
2023-10-07 12:35:26 +0400

    Update Get-MaxKSize.ps1
    Updations for plots upto K40

--- PSChiaPlotter/Private/Get-MaxKSize.ps1
+++ PSChiaPlotter/Private/Get-MaxKSize.ps1
@@ -1,8 +1,8 @@
 function Get-MaxKSize {
     [CmdletBinding()]
     param(
-        [ValidateSet("K32","K33","K34","K35")]
-        [string[]]$KSize = ("K32","K33","K34","K35"),
+        [ValidateSet("K32","K33","K34","K35","K36","K37","K38","K39","K40")]
+        [string[]]$KSize = ("K32","K33","K34","K35","K36","K37","K38","K39","K40"),
 
         [Parameter(Mandatory)]
         [int64]$TotalBytes
